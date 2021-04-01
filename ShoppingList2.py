6ea37f6568dafc57f5714c124e4b926b1b12b899
jaredsylvia <jaredsylvia@gmail.com>
2021-04-01 00:07:15 -0700

    Create ShoppingList2.py

---
+++ ShoppingList2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 22:41:36 2021
+
+@author: jared
+"""
+import matplotlib.pyplot as plt
+
+def addItem():
+    shoppingList = {'item' : [], 'qty' : [], 'price' : [], 'total' : []}
+    while True:
+        appendingItem = input('Item: ')
+        if appendingItem == 'done':
+            return(shoppingList)
+        itemQty = int(input('Quantity: '))
+        itemPrice = float(input('Unit price: '))
+        shoppingList['item'].append(appendingItem)
+        shoppingList['qty'].append(itemQty)
+        shoppingList['price'].append(itemPrice)
+        shoppingList['total'].append(itemPrice * itemQty)
+       
+def getTotals(val): 
+    return val['total']    
+
+
+
+print('Create and display a shopping list.')
+print('Enter done for name of last item on list.')
+
+shoppingList = addItem();
+
+plt.pie(shoppingList['total'], labels = shoppingList['item'], startangle=45, shadow = True, autopct = '%1.0f%%')
+plt.title('Allocation of your $' + str(sum(shoppingList['total'])))
+plt.show()
+
+plt.pie(shoppingList['qty'], labels = shoppingList['item'], startangle=45, shadow = True, autopct = '%1.0f%%')
+plt.title('Total Individual Items')
+plt.show()
+
+
+
+
+
+
+    
+
